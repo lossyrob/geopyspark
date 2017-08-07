3228a64fcd10fa742c24607f7427ecc914f2e224
lossyrob <rdemanuele@gmail.com>
2017-08-07 19:03:33 -0400

    Use MultibandTile instead of Tile for parts of TMS server

--- geopyspark-backend/project/Version.scala
+++ geopyspark-backend/project/Version.scala
@@ -1,6 +1,6 @@
 object Version {
   val geopyspark = "0.2.0"
-  val geotrellis = "1.2.0-SNAPSHOT"
+  val geotrellis = "1.2.0-ROB-WORKSHOP"
   val scala       = "2.11.11"
   val crossScala  = Seq("2.11.11", "2.10.6")
   val scalaTest   = "2.2.0"
